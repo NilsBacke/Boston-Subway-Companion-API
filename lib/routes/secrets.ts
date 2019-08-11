40c3f1f9941bab75a906f8c75d227f55d5470503
Nils Backe <nils.backe@gmail.com>
2019-08-10 21:52:04 -0400

    adding secrets in env

--- lib/routes/secrets.ts
+++ lib/routes/secrets.ts
@@ -1,3 +1,5 @@
 export const apiKey = 'dc44b30101114e88b45041a4a9b65e06'
 export const baseURL = 'https://api-v3.mbta.com'
 export const awsApiKey = 'q9sdm8YIPS2M8wf3frWic56cqZ4WSIuM4NNnYoKf'
+export const distanceMatrixKey = 'AIzaSyAWdYvAyOYBjqqlTDQCKYCn9psNJkNHR_g'
+export const directionsKey = 'AIzaSyAc0KTXVwsaicsJ7f9ggEu6AmIl1JO_BQg'
