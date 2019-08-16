abc743f2805c78081125abca78e687296b55e870
Nils Backe <nils.backe@gmail.com>
2019-08-15 22:03:04 -0400

    0,1

--- lib/routes/neighborStop.ts
+++ lib/routes/neighborStop.ts
@@ -8,7 +8,7 @@ export async function neighborStop(stopId: string): Promise<string> {
 	try {
 		const stopIdNum: number = Number(stopId)
 
-		const url = `${baseURL}/stops?api_key=${apiKey}&filter[route_type]=1&filter[id]=${stopIdNum},${stopIdNum -
+		const url = `${baseURL}/stops?api_key=${apiKey}&filter[route_type]=0,1&filter[id]=${stopIdNum},${stopIdNum -
 			1},${stopIdNum + 1}`
 		// stopID, stopID - 1, stopID + 1
 		const json = await executeCall(url)
