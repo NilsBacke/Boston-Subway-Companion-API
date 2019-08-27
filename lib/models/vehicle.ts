03dd142d9f5d0e61c94848312b450cbbf6e66b71
Nils Backe <nils.backe@gmail.com>
2019-08-26 22:30:00 -0400

    remove cache

--- lib/models/vehicle.ts
+++ lib/models/vehicle.ts
@@ -33,7 +33,7 @@ export function makeVehicle(jsonResponse: any, index: number): Vehicle {
 		bearing: attributes.bearing,
 		latitude: attributes.latitude,
 		longitude: attributes.longitude,
-		speed: parseFloat((attributes.speed as number).toFixed(1)),
+		speed: parseFloat(attributes.speed.toFixed(1)),
 		updatedAt: attributes.updated_at,
 		lineName: routeId + ' Line',
 		directionDestination: directionDestination,
