d23ccc5bf40545359f380c3dc54ca57196756b3e
Nils Backe <nils.backe@gmail.com>
2019-08-26 22:47:28 -0400

    +.0

--- lib/models/vehicle.ts
+++ lib/models/vehicle.ts
@@ -33,7 +33,7 @@ export function makeVehicle(jsonResponse: any, index: number): Vehicle {
 		bearing: attributes.bearing,
 		latitude: attributes.latitude,
 		longitude: attributes.longitude,
-		speed: attributes.speed == null ? null : parseFloat(attributes.speed.toFixed(1)),
+		speed: attributes.speed == null ? null : parseFloat(attributes.speed.toString() + '.0'),
 		updatedAt: attributes.updated_at,
 		lineName: routeId + ' Line',
 		directionDestination: directionDestination,
