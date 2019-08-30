bb6b25959ff0614f1c8643255317de3b4db39193
Nils Backe <nils.backe@gmail.com>
2019-08-30 11:07:59 -0400

    adding bussses

--- lib/routes/allNearby.ts
+++ lib/routes/allNearby.ts
@@ -3,12 +3,17 @@ import { baseURL, apiKey } from '.'
 
 const rangeInMiles = 1000
 
-export async function allNearby(locationData: any, range?: number): Promise<string> {
-	const radius = 0.02 * (range || rangeInMiles)
+export async function allNearby(
+    locationData: any,
+    range?: number
+): Promise<string> {
+    const radius = 0.02 * (range || rangeInMiles)
 
-	const url = `${baseURL}/stops?api_key=${apiKey}&filter[latitude]=${locationData!.latitude}&filter[longitude]=${
-		locationData!.longitude
-	}&filter[radius]=${radius}&filter[route_type]=0,1&sort=distance`
+    const url = `${baseURL}/stops?api_key=${apiKey}&filter[latitude]=${
+        locationData!.latitude
+    }&filter[longitude]=${
+        locationData!.longitude
+    }&filter[radius]=${radius}&filter[route_type]=0,1,3&sort=distance`
 
-	return (await handleMultipleStops(url, true)) as string
+    return (await handleMultipleStops(url, true)) as string
 }
